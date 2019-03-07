04972b50967de5e4014a4995e39281621f37d4ac
rhianna larocque <rhiannamrl@gmail.com>
2019-03-07 15:22:01 -0500

    laying out of the layout

--- src/Landing.js
+++ src/Landing.js
@@ -5,6 +5,24 @@ export default class Landing extends Component {
   render() {
     return (
       <div>
+        <div className="Nav-bar">
+          <div className="Container">
+            <h1>Navbar</h1>
+          </div>
+        </div>
+
+        <div className="Computer-hero">
+          <div className="Container">
+            <h1>Computer Image</h1>
+          </div>
+        </div>
+
+        <div className="Partners">
+          <div className="Container">
+            <h1>Partners</h1>
+          </div>
+        </div>
+
         <div className="Solution">
           <div className="Container">
             <h1>Choose the solution that's right for your business</h1>
@@ -56,30 +74,28 @@ export default class Landing extends Component {
           </div>
         </div>
 
-        <div>
-          <div className="Footer">
-            <div className="Container">
-              <ul className="Footer-links">
-                <li className="Footer-item">SURE.</li>
-                <li className="Footer-item">Privacy Policy</li>
-                <li className="Footer-item">Terms of Service</li>
-                <li className="Footer-item">Licenses</li>
-                <li className="Footer-item">Products</li>
-                <li className="Footer-item">Support</li>
-                <li className="Footer-item">Contact Us</li>
-              </ul>
-              <ul className="Footer-social">
-                <li className="Footer-social-item">
-                  <i class="fab fa-twitter" />
-                </li>
-                <li className="Footer-social-item">
-                  <i class="fab fa-facebook-f" />
-                </li>
-                <li className="Footer-social-item">
-                  <i class="fab fa-instagram" />
-                </li>
-              </ul>
-            </div>
+        <div className="Footer">
+          <div className="Container">
+            <ul className="Footer-links">
+              <li className="Footer-item">SURE.</li>
+              <li className="Footer-item">Privacy Policy</li>
+              <li className="Footer-item">Terms of Service</li>
+              <li className="Footer-item">Licenses</li>
+              <li className="Footer-item">Products</li>
+              <li className="Footer-item">Support</li>
+              <li className="Footer-item">Contact Us</li>
+            </ul>
+            <ul className="Footer-social">
+              <li className="Footer-social-item">
+                <i class="fab fa-twitter" />
+              </li>
+              <li className="Footer-social-item">
+                <i class="fab fa-facebook-f" />
+              </li>
+              <li className="Footer-social-item">
+                <i class="fab fa-instagram" />
+              </li>
+            </ul>
           </div>
         </div>
       </div>
