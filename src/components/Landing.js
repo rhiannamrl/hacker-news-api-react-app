d622f09dc7fc7209fc56112bb4b39168492d8717
rhianna larocque <rhiannamrl@gmail.com>
2019-03-09 15:30:37 -0500

    updated css

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
+
 import '../css/landing.css'
 import SURElogo from '../images/SURElogo.png'
 import SURElogoBlue from '../images/SURElogoBlue.png'
 import AllRights from '../images/AllRights.png'
+import Computer from '../images/Computer.png'
+import chub from '../images/chub.png'
+import Nationwide from '../images/Nationwide.png'
+import qbe from '../images/qbe.png'
+import Assurant from '../images/Assurant.png'
+import API from '../images/API.png'
+import iFrame from '../images/iFrame.png'
+import WhiteLabel from '../images/WhiteLabel.png'
 
 export default class Landing extends Component {
   render() {
@@ -28,41 +37,76 @@ export default class Landing extends Component {
               <ul>
                 <li>Download App</li>
                 <li>Login</li>
-                <li>
-                  <button>Sign Up</button>
-                </li>
+                <li>Sign Up</li>
               </ul>
             </div>
           </nav>
-        </div>
-
-        <div className="Computer-hero">
-          <div className="Container">
-            <h1>Computer Image</h1>
+          <div className="Get-started">
+            <h1>Sure + You</h1>
+            <h2>
+              Complement your core business and{' '}
+              <b>increase customer lifetime value</b> with integrated offers
+              from top-tier insurance carriers.
+            </h2>
+            <button>Get Started</button>
           </div>
-        </div>
-
-        <div className="Partners">
-          <div className="Container">
-            <h1>Partners</h1>
+          <div className="Computer-Image">
+            <img src={Computer} alt={'comp'} />
           </div>
-        </div>
-
-        <div className="Solution">
-          <div className="Container">
-            <h1>Choose the solution that's right for your business</h1>
+          <div />
+          <div className="Partners">
+            <img src={chub} alt={'chub'} />
+            <img src={Nationwide} alt={'nationwide'} />
+            <img src={qbe} alt={'qbe'} />
+            <img src={Assurant} alt={'assurant'} />
+          </div>
+          <div className="Solution">
+            <div className="Solution-header">
+              <h1>Choose the solution that's right for your business</h1>
+            </div>
+            <div className="Solutions">
+              <aside>
+                <h4>
+                  <img src={API} alt={'api'} />
+                  API
+                </h4>
+                <h5>
+                  The Sure API is a set of RESTul enpoints that allow you to
+                  customize the look and feel for a truly native experience
+                </h5>
+              </aside>
+              <aside>
+                <h4>
+                  <img src={iFrame} alt={'iFrame'} />
+                  iFrame
+                </h4>
+                <h5>
+                  The iFrame solution allows for easy integration - imply a
+                  place a line of code to embed an insurance form anywhere on
+                  your sight.
+                </h5>
+              </aside>
+              <aside>
+                <h4>
+                  <img src={WhiteLabel} alt={'white label'} />
+                  White Label
+                </h4>
+                <h5>
+                  Sure will build and host a branded page, which you can link to
+                  from a webpage or an email.
+                </h5>
+              </aside>
+            </div>
           </div>
         </div>
 
         <div>
           <div className="Policy-hero">
-            <div className="Container">
-              <div className="Policy-hero-wrapper">
-                <h1>
-                  Our partners come in all shapes and sizes, just like our
-                  policies
-                </h1>
-              </div>
+            <div className="Policy-hero-wrapper">
+              <h1>
+                Our partners come in all shapes and sizes, just like our
+                policies
+              </h1>
             </div>
           </div>
         </div>
