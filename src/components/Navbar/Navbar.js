0eb1782c8a6bc45a23d65769f2d7aaaca7b3bdfe
rhianna larocque <rhiannamrl@gmail.com>
2019-03-11 11:26:14 -0400

    mobile menu update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,29 +6,39 @@ import SURElogo from './assets/SURElogo.png'
 class Navbar extends Component {
   render() {
     return (
-      <nav>
-        <div>
-          <ul>
-            <li>
-              <img src={SURElogo} alt={'logo'} />
-            </li>
-            <li>
-              <span>Insurance</span>
-              <i class="fas fa-caret-down" />
-            </li>
-            <li>Products</li>
-            <li>Support</li>
-            <li>Claims</li>
-          </ul>
+      <div>
+        <nav>
+          <div className="Desktop-menu">
+            <ul>
+              <li>
+                <img src={SURElogo} alt={'logo'} />
+              </li>
+              <li>
+                <span>Insurance</span>
+                <i class="fas fa-caret-down" />
+              </li>
+              <li>Products</li>
+              <li>Support</li>
+              <li>Claims</li>
+            </ul>
+          </div>
+          <div className="Desktop-menu">
+            <ul>
+              <li>Download App</li>
+              <li>Login</li>
+              <li>Sign Up</li>
+            </ul>
+          </div>
+        </nav>
+        <div className="Mobile-menu">
+          <div class="Mobile-logo">
+            <img src={SURElogo} alt={'logo'} />
+          </div>
+          <div class="Navbar__Link-toggle Navbar__Link-toggle">
+            <i class="fas fa-bars" />
+          </div>
         </div>
-        <div>
-          <ul>
-            <li>Download App</li>
-            <li>Login</li>
-            <li>Sign Up</li>
-          </ul>
-        </div>
-      </nav>
+      </div>
     )
   }
 }
